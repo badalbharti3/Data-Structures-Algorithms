e4e7f0c183a3d68a17136bf3f01f90172c353ef2
Badal <badalbharti5@gmail.com>
2025-09-22 21:43:49 +0530

    Smallest and Second Smallest in an array

---
+++ Arrays/smallestNum.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class smallestNum {
+    static void smallest(int[] arr, int n){
+        int min = arr[0];
+        int s_min = Integer.MAX_VALUE;
+        for(int i:arr){
+            if(i < min){
+                s_min = min;
+                min = i;
+            }
+            else if(i != min && i < s_min){
+                s_min = i;
+            }
+        }
+        if(s_min == Integer.MAX_VALUE) s_min = -1;
+        System.out.println("Smallest: "+min+" Second Smallest: "+s_min);
+    }
+
+    
+   public static void main(String[] args) {
+    int arr[] = {1,1,1,1,1};
+    int n = arr.length;
+
+    smallest(arr,n);
+    // secondSmallest(arr,n);
+   }
+}
